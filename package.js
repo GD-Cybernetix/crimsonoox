9d84ef6d9ecc99e4e2b0dedef8db34647b33548c
GD-Cybernetix <154467176+GD-Cybernetix@users.noreply.github.com>
2023-12-20 18:50:37 -0500

    Create package.js

---
+++ package.js
@@ -0,0 +1,21 @@
+{
+    "name": "crimsonoox",
+    "version": "1.0.0",
+    "description": "",
+    "scripts": {
+        "build": "echo \"No build specified\" && exit 1",
+        "test": "echo \"No test specified\" && exit 1",
+        "start": "node index.js"
+    },
+    "repository": {
+        "type": "git",
+        "url": "git+https://github.com/GD-Cybernetix/crimsonoox.git"
+    },
+    "author": "",
+    "license": "ISC",
+    "bugs": {
+        "url": "https://github.com/GD-Cybernetix/crimsonoox/issues"
+    },
+    "homepage": "https://github.com/GD-Cybernetix/crimsonoox#readme"
+}
+ 
